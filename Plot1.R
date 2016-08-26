9445876503f0322694b3ba8f8a2d63259a26e903
danwilliams39 <danwilliams39@gmail.com>
2016-08-26 08:14:02 -0700

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,13 @@
+install.packages("gridExtra")
+library(gridExtra)
+
+
+unzip("Data.2.zip")
+class.code=readRDS("Source_Classification_Code.rds")
+summary.pm25=readRDS("summarySCC_PM25.rds")
+master=merge(class.code[,c("SCC","SCC.Level.One","SCC.Level.Two","SCC.Level.Three")],summary.pm25,by.x="SCC",by.y = "SCC")
+year.table=with(master,tapply(Emissions,year,sum))
+
+#plot 1: Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?
+plot(year.table,type='l',xlab="Year",ylab="Total PM2.5 Emissions",lwd=3,col="navy",main="Total PM2.5 Emissions in United States",xaxt='n')
+axis(1,at=seq(1:4),labels=rownames(year.table))
